6f1b499df7b0e1f31ecd73d60ba8502b2a89fdcc
louisesykes198 <louisesykes198@yahoo.com>
2025-06-19 07:23:30 +0100

    amending keys

--- env.py
+++ env.py
@@ -2,8 +2,8 @@ import os
 
 os.environ.setdefault('MY_ENVIRONMENT_VARIABLE', 'variable_value')
 
-os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51RXgVO4EprSnJzaKaXpXGWvkOnZDZQgxdrHzNKQdMVGyG70zBlkzh0quu3dnY60cCSsQm1DWu3bac8hdswfSctBO00MZAUAAj9"
-os.environ["STRIPE_SECRET_KEY"] = "sk_test_51RXgVO4EprSnJzaKtYL7dmbqHEUeEZR2HUHi4TGBFMkLH9O0sa5UMWbDaEALl2Ayxmf2QuLfWxu8Z4aLQebNPItb000T2bKmc7"
+os.environ["STRIPE_PUBLIC_KEY"] = 'pk_test_51LsAVSDbrSAzYrzcRSLdMAqeaXZkUNLmV0cXhl70xgfdimMu6ZPgf6orKSjM3KQR2JfEfxPjISBRhUV6xXX5WnyA00ZksvPBi2'
+os.environ["STRIPE_SECRET_KEY"] = 'sk_test_51LsAVSDbrSAzYrzcRSLdMAqeaXZkUNLmV0cXhl70xgfdimMu6ZPgf6orKSjM3KQR2JfEfxPjISBRhUV6xXX5WnyA00ZksvPBi2'
 
 
 
