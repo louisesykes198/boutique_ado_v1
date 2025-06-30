f4cf09ad4b4f65c9baf93eeab741f036b94e5c39
louisesykes198 <louisesykes198@yahoo.com>
2025-06-30 12:48:05 +0100

    059 - deployment part 5

---
+++ env.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ.setdefault('MY_ENVIRONMENT_VARIABLE', 'variable_value')
+
+os.environ["STRIPE_PUBLIC_KEY"] = 'pk_test_51RXgVO4EprSnJzaKaXpXGWvkOnZDZQgxdrHzNKQdMVGyG70zBlkzh0quu3dnY60cCSsQm1DWu3bac8hdswfSctBO00MZAUAAj9'
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51RXgVO4EprSnJzaKtYL7dmbqHEUeEZR2HUHi4TGBFMkLH9O0sa5UMWbDaEALl2Ayxmf2QuLfWxu8Z4aLQebNPItb000T2bKmc7"
+os.environ.setdefault('STRIPE_WH_SECRET', 'whsec_eb25deb6f388ff903ca95074bcb78d0a394157398b21546c830bbdcabeebe19e')
+os.environ.setdefault('DEVELOPMENT', '1')
+
+
